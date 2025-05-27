dbe5f708772e9b038f722c9a8fc163a2311330e6
Avishka426 <pathumwghm@gmail.com>
2025-05-28 00:22:00 +0530

    Chat port change

--- components/MessageComponent.js
+++ components/MessageComponent.js
@@ -113,7 +113,7 @@ const MessageComponent = ({ onClose }) => {
   // WebSocket connection
   useEffect(() => {
     // Establish WebSocket connection
-    const ws = new WebSocket(`ws://localhost:9091/ws/chat`);
+    const ws = new WebSocket(`ws://localhost:9090/ws/chat`);
 
     ws.onopen = () => {
       console.log('WebSocket Connected');
